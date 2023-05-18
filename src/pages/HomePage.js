08624aa834d168e1a8b780a3b5baeb67ef1ff72a
sophokel <s_hurricane@gmx.net>
2023-05-18 07:56:28 +0200

    remove loading spinner from front page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,8 @@
-import Button from "@mui/material/Button";
-import { ArrowForward } from "@mui/icons-material";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { CircularProgress } from "@mui/material";
-import { StyledMainButton } from "../components/StyledMainButton";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
-  const [showSpinner, setShowSpinner] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -16,39 +12,22 @@ function LandingPage() {
   const handleClick = (e) => {
     e.preventDefault();
 
-    setShowSpinner(true);
-
     setTimeout(() => {
-      setShowSpinner(false);
       navigate("/information");
     }, 3000);
   };
 
   return (
     <div className="landing-page">
-      {showSpinner && (
-        <div className="snackbar snackbar-info snackbar-bottom-home row">
-          <CircularProgress size="1rem" sx={{ color: "white" }} />
-          <p className="snackbar-text">Applikation lädt...</p>
-        </div>
-      )}
       <img
         src="/images/kindergarten_text.png"
         alt="Kindergarten"
         style={{ width: "300px" }}
       ></img>
       <br />
-      <StyledMainButton
-        variant="contained"
-        disabled={showSpinner}
-        startIcon={<ArrowForward />}
-        sx={{
-          width: 250,
-        }}
-        onClick={handleClick}
-      >
+      <Link className="nav-link" onClick={handleClick}>
         Zur Kindergartensuche
-      </StyledMainButton>
+      </Link>
     </div>
   );
 }
